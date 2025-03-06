e845729ed9392d77648a26485bbc45be390e9607
edenWerke <163598468+edenWerke@users.noreply.github.com>
2025-03-06 10:03:06 +0300

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,3 +1,46 @@
+// import React from "react";
+// import Card from "react-bootstrap/Card";
+// import { ImPointRight } from "react-icons/im";
+
+// function AboutCard() {
+//   return (
+//     <Card className="quote-card-view">
+//       <Card.Body>
+//         <blockquote className="blockquote mb-0">
+//           <p style={{ textAlign: "justify" }}>
+//             Hello there, I am <span className="purple">Eden Werke </span>
+//             <br />
+//              pursuing a Bachelor’s degree in <span className="purple">Software Engineering </span>at Mekelle University.
+//             <br />
+//             {/* I have completed Integrated MSc (IMSc) in Maths and Computing at BIT */}
+ 
+//             <br />
+//             <br />
+//             Apart from coding, some other activities that I love to do!
+//           </p>
+//           <ul>
+//             <li className="about-activity">
+//               <ImPointRight /> solving puzzles 
+//             </li>
+//             <li className="about-activity">
+//               <ImPointRight />Chilling with Friends
+//             </li>
+//             <li className="about-activity">
+//               <ImPointRight /> Travelling
+//             </li>
+//           </ul>
+
+//           <p style={{ color: "rgb(155 126 172)" }}>
+//           "Every expert was once a beginner who practiced... a lot!"{" "}
+//           </p>
+//           <footer className="blockquote-footer">Malcolm Gladwell</footer>
+//         </blockquote>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default AboutCard;
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
@@ -8,22 +51,19 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hello there, I am <span className="purple">Eden Werke </span>
-            <br />
-             pursuing a Bachelor’s degree in <span className="purple">Software Engineering </span>at Mekelle University.
-            <br />
-            {/* I have completed Integrated MSc (IMSc) in Maths and Computing at BIT */}
- 
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
+            Hello there, I am <span className="purple">Eden Werke</span>, 
+            pursuing a Bachelor’s degree in <span className="purple">Software Engineering</span> 
+            at Mekelle University.
+          </p>
+          <p style={{ textAlign: "justify", marginTop: "10px" }}>
+            Apart from coding, some other activities that I love to do:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> solving puzzles 
+              <ImPointRight /> solving puzzles
             </li>
             <li className="about-activity">
-              <ImPointRight />Chilling with Friends
+              <ImPointRight /> Chilling with friends
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -31,7 +71,7 @@ function AboutCard() {
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-          "Every expert was once a beginner who practiced... a lot!"{" "}
+            "Every expert was once a beginner who practiced... a lot!"
           </p>
           <footer className="blockquote-footer">Malcolm Gladwell</footer>
         </blockquote>
