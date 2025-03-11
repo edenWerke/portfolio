60e184e8341032c33bf2ed563c8d4ba56c7d6743
edenWerke <163598468+edenWerke@users.noreply.github.com>
2025-03-11 08:17:09 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Design Inspired by Soumyajit Behera</h3>
+          <h3> Inspired by Soumyajit Behera</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} Eden Werke</h3>
