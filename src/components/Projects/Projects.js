3c2675837c7639c1470ee27e8c93743c400c30b2
edenWerke <edenw6292@gmail.com>
2026-06-12 16:35:59 +0300

    add .

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,13 +3,13 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import book from "../../Assets/Projects/BOOK.png";
-import shoe from "../../Assets/Projects/SHOEE.png";
+import shoe from "../../Assets/Projects/medical.png";
 import toDo from "../../Assets/Projects/TODO.png";
 import foood from "../../Assets/Projects/foood.png";
 import bank from "../../Assets/Projects/bank.png";
 
-import rate from "../../Assets/Projects/rate3.jpg";
-import invent from "../../Assets/Projects/inn.jpg";
+import rate from "../../Assets/Projects/ride.png";
+import invent from "../../Assets/Projects/inn.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -22,6 +22,41 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+           <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={invent}
+              isBlog={false}
+              title=" AI Book Chat Platform"
+              description="an AI-powered platform that transforms static PDFs into real-time conversational experiences. Implemented natural voice synthesis, ElevenLabs persona previews, and secure authentication. Users can extract text from PDFs, interact with their content via voice, and access session transcripts, creating a seamless, dynamic library experience."
+
+
+              ghLink="https://github.com/edenWerke/Bookiefied.git"
+              // demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+
+ <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={shoe}
+              isBlog={false}
+              title="Care Pluse"
+              description="a healthcare platform that streamlines patient registration, appointment scheduling, and medical records management. Designed and implemented complex forms, interactive UI components, and Email notifications to enhance user experience. Ensured responsive design and usability across devices, improving efficiency for patients and clinic staff. Learned and applied modern front-end best practices while integrating dynamic features"
+              ghLink="https://github.com/edenWerke/CarePluse.git"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col>
+
+<Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={rate}
+              isBlog={false}
+              title="Real-Time Rides & Payments"
+              description="a full-stack Uber clone using React Native and Expo, enabling real-time ride tracking, driver-rider matching, and secure payments. Leveraged edge-ready features for fast performance and seamless user experience. Delivered a scalable, high-performance app that mimics core Uber functionality while showcasing modern mobile development skills."
+              ghLink="https://github.com/edenWerke/Uber-ride.git"
+              // demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={bank}
@@ -34,16 +69,7 @@ function Projects() {
           </Col>
 
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={shoe}
-              isBlog={false}
-              title="Shoe Shop App"
-              description="TThe Shoe Shop App is a user-friendly platform designed for an enhanced online footwear shopping experience. Users can easily browse a variety of shoe styles, including sneakers, boots, and sandals, using an intuitive search function. The app provides detailed product descriptions, high-quality images, and customer reviews to aid decision-making."
-              ghLink="https://github.com/edenWerke/Shoe-shop-app.git"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+         
 
 
 
@@ -62,32 +88,10 @@ function Projects() {
 
 
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={invent}
-              isBlog={false}
-              title="Inventory Tracking System"
-              description="The Inventory Management System developed using PHP is a robust solution designed to streamline the tracking and management of stock levels. This web-based application enables users to efficiently monitor inventory across multiple locations, with real-time updates on stock availability.
-
-Key features include adding and editing product details, tracking stock movements."
-              ghLink="https://github.com/edenWerke/ims.git"
-              // demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-
+         
           
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={rate}
-              isBlog={false}
-              title="Donate-Blood-App"
-              description="The Donate App created with Vue is a user-friendly platform designed to facilitate charitable giving. Users can easily browse various causes and organizations, make donations, and track their contributions over time. The app features a clean and responsive design, making navigation seamless on any device."
-              ghLink="https://github.com/edenWerke/Donate-blood.git"
-              // demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
+          
 
 
           <Col md={4} className="project-card">
